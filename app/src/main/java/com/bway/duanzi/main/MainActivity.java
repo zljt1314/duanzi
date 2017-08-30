bda200c9c2391c75bc714ad7edc025debcf31d06
LeeDaDaDa <921093571@qq.com>
2017-08-30 14:49:26 +0800

    77

--- app/src/main/java/com/bway/duanzi/main/MainActivity.java
+++ app/src/main/java/com/bway/duanzi/main/MainActivity.java
@@ -1,19 +1,17 @@
 package com.bway.duanzi.main;
+package com.bway.duanzi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.bway.duanzi.R;
-
 public class MainActivity extends AppCompatActivity {
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
 
     }
 }
