6fec0288d0fd8c282fed647218533dc3440a7919
lenovo <962229493@qq.com>
2017-08-30 09:36:03 +0800

    11

--- app/src/main/java/com/bway/duanzi/MainActivity.java
+++ app/src/main/java/com/bway/duanzi/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //ddd
-    //
+    //lijintao
     //李开虎
     @Override
     protected void onCreate(Bundle savedInstanceState) {
